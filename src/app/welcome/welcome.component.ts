726fa7558ca26decab280da9e4d0cd066e20305d
jacklusy <jackalloussi23@gmail.com>
2024-09-26 13:07:15 +0300

    Welcome Component

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './welcome.component.css'
 })
 export class WelcomeComponent {
+  userName: string = 'Hazem';
+  profile = {
+    name: 'Ahmad Azmi',
+    position: 'Junior Accounting Officer',
+    supervisor: 'Salwa Assem',
+    level: 'Level 10'
+  };
+  requestsCount: number = 10;
+  searchText: string = '';  
+  isProfileVisible: boolean = true; 
 
+  toggleProfileView() {
+    this.isProfileVisible = !this.isProfileVisible;
+  }
 }
